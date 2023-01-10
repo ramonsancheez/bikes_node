690e2c51f9c46b560a50d2588c5a2476c76faf08
Ramon <rsanchezabadia@cifpfbmoll.eu>
2023-01-10 20:10:06 +0100

    feat: :heavy_plus_sign: installing middleware body-parser

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bikesRouter = require('./routes/routes.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
+require('./middleware/bodyParser')(app);
+
 app.use('/', bikesRouter);
 
 app.listen(port, () => console.log('server running at port', port));
