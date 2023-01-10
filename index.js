38af9f3d10aded21996b566c2017b7e3f42a2fb3
Ramon <rsanchezabadia@cifpfbmoll.eu>
2023-01-10 19:41:16 +0100

    refactor: :zap: app.listen performance

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const mongoose = require('./config/db.connection.js');
 const bikesRouter = require('./routes/routes.js');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use('/', bikesRouter);
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-});
+app.listen(port, () => console.log('server running at port', port));
