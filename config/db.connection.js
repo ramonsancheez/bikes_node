c11e7489f6468de3f89105aa507753c158252db0
Ramon <rsanchezabadia@cifpfbmoll.eu>
2023-01-10 20:31:48 +0100

    revert: :construction: wip - put mongo uri to avoid errors

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('mongodb+srv://ramonsancheez:Rjq0cz5WeXXmCGKZ@cluster0.9rfuhuh.mongodb.net/bikestore?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
