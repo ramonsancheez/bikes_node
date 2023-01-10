eab593fd723b897b3add926cc7bc30469d5927ad
Ramon <rsanchezabadia@cifpfbmoll.eu>
2023-01-10 19:39:25 +0100

    fix: :lock: fix mongo uri

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const uri = 'mongodb+srv://ramonsancheez:Rjq0cz5WeXXmCGKZ@cluster0.9rfuhuh.mongodb.net/bikestore?retryWrites=true&w=majority'
 
-mongoose.connect(uri, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
