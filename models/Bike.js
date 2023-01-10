3bf1d9ff57d4aca5b5d2047625908f83e06ad86e
Ramon <rsanchezabadia@cifpfbmoll.eu>
2023-01-10 19:55:58 +0100

    feat: :sparkles: adding mongoose schema

---
+++ models/Bike.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const BikeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    }
+});
+
+const Bike = mongoose.model('Bike', BikeSchema);
+
+module.exports = Bike;
