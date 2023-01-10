bfda9d463e133aabd3c483b8555a28a1d304293b
Ramon <rsanchezabadia@cifpfbmoll.eu>
2023-01-10 19:56:30 +0100

    feat: :sparkles: adding READ (get) method

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const Bike = require('../models/bike');
 
 router.get('/', async (req, res) => {
     res.json('Bienvendio a la tienda de bicis!');
 });
+
+// GET
+    router.get('/bikes/:id', async (req, res) => {
+        try {
+            const bike = await Bike.findById(req.params.id);  
+            res.json(bike);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    });
+
+module.exports = router;
