defc1478ebfdf7f531a6d0e14317f83630a012d3
Ramon <rsanchezabadia@cifpfbmoll.eu>
2023-01-15 13:36:26 +0100

    refactor: :recycle: deleting unnecessary variables

--- config/db.connection.js
+++ config/db.connection.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
+const db_connection = mongoose.connection;
 
 mongoose.connect('mongodb+srv://ramonsancheez:Rjq0cz5WeXXmCGKZ@cluster0.9rfuhuh.mongodb.net/bikestore?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-const db = mongoose.connection;
+db_connection.on('error', console.error.bind(console, 'connection error:'));
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db_connection.once('open', function() {
   console.log('Conectado a Mongo!');
 });
 
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
-require('./config/db.connection.js');
 const app = express();
 const routes = require('./routes/routes');
 const port = process.env.PORT || 3000;
-
 app.use(express.json())
 
+require('./config/db.connection.js');
 app.use('/', routes)
 
 app.listen(port, () => console.log('server running at port', port));
