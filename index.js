cfe4f35d5297ac566189f50d1784a241f85decae
Ramon <rsanchezabadia@cifpfbmoll.eu>
2023-01-18 01:19:05 +0100

    refactor: :zap: dividing file routes into two different files

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes/routes');
+const bikeRoutes = require('./routes/bike.routes.js');
+const storeRoutes = require('./routes/store.routes.js');
 app.use(express.json())
 
 require('./config/db.connection.js');
-app.use('/', routes)
+app.use('/', bikeRoutes)
+app.use('/store', storeRoutes)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('server running at port', port));
