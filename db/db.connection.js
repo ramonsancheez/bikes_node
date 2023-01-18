dfbbb953525b9f6cf497e27457f86fc86b545b1d
Ramon <rsanchezabadia@cifpfbmoll.eu>
2023-01-18 18:12:44 +0100

    refactor: :fire: removing bd unnecessary code

--- db/db.connection.js
+++ db/db.connection.js
@@ -12,11 +12,4 @@ db_connection.once('open', function() {
   console.log('Conectado a Mongo!');
 });
 
-process.on('SIGINT', function() {  
-  mongoose.connection.close(function () { 
-    console.log('La conexión ha finalizado'); 
-    process.exit(0); 
-  }); 
-});
-
 module.exports = mongoose;
